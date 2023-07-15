a98b95cf4d67e8bd234e795047659d966d112c35
Eugene Serb <46799701+eugene-serb@users.noreply.github.com>
2023-07-15 17:17:57 +0300

    #37 update file structure, update tests, update webpack config

---
+++ src/pages/index/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import Game2048 from '@/game/2048.js';
+
+const params = {
+  speedRate: 200,
+  keyRating: 'es:2048',
+};
+
+const game2048 = new Game2048(params);
+
+game2048.start();
